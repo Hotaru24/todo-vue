5a40749de28296ede01cef5abecfcf8162129e35
Hotaru24 <florent-pasquet@protonmail.com>
2021-03-15 20:09:31 +0100

    delete todo is ok

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -3,7 +3,7 @@
     <div class="card-body" >
       <h5 class="card-title">{{title}}</h5>
       <p class="card-text"> {{describe}}</p>
-      <button href="#" class="btn btn-danger" onClick={deleteTodo}>Delete</button>
+      <button href="#" class="btn btn-danger" v-on:click="deleteTodo">Delete</button>
     </div>
   </div>
 </template>
@@ -14,20 +14,22 @@ export default {
   name: 'Todo',
 	props: {
     title: String,
-		describe: String
+		describe: String,
+    index: Number
   },
 	data: function (){
     return {
-      data: []
+      dataTodo: [],
+      todoIndex: this.index
      }
     },
     mounted: function(){
-      localStorage.clear();
-      this.data = this.$store.state.todoData;
+      this.dataTodo = this.$store.state.storeData;
     },
 		methods: {
 			deleteTodo(){
-				return this.$store.commit('todos', this.data)	 
+        this.dataTodo.splice(this.todoIndex, 1);
+				return this.$store.commit('setStoreData', this.dataTodo)	 
 			}
 		}
 
